c957f931eccd6bab9c240060cfa490ab401c8399
Stephyyy03 <mendezstephen26@gmail.com>
2022-10-26 18:31:08 +0800

    updated design

--- home.php
+++ home.php
@@ -13,8 +13,8 @@ if (empty($_SESSION['email'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bs-5/bootstrap/dist/css/bootstrap.css">
-    <title>Coozy Apparel.</title>
-</head>
+    <title>Coozy Apparel</title>
+</head> 
 <style>
     @media screen and (width: 992) {
         .card-wrapper{
@@ -26,7 +26,7 @@ if (empty($_SESSION['email'])){
     <nav class="navbar navbar-expand-lg navbar-dark bg-black">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="../ecommerce/src/img/logo.png" width="160" alt="" sizes="" srcset="">
+                <img src="../ecommerce/src/img/logo.png" width="150" alt="" sizes="" srcset="">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -34,19 +34,35 @@ if (empty($_SESSION['email'])){
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto" style="font-size: 20px;">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Shop</a>
+                        <a class="nav-link" href="#">SHOP</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Cart</a>
+                        <a class="nav-link" href="#">CART</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Products</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
+                            PRODUCTS
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">New Arrival</a>
+                            <a class="dropdown-item" href="#">Sale</a>
+                            <a class="dropdown-item" href="#">Hot</a>
+                            <a class="dropdown-item" href="#">T-Shirts</a>
+                            <a class="dropdown-item" href="#">Bags</a>
+                        </div>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Order Status</a>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#">ORDER STATUS</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="logout.php">Log Out</a>
+                    <li class="nav-item dropdown align-items-center d-flex">
+                        <a class="nav-link py-0 dropdown-toggle" href="#" id="navbarDropdown" data-bs-toggle="dropdown"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                        </svg></a>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="settings.html">Settings</a>
+                            <a class="dropdown-item" href="logout.php">Logout</a>
+                        </div>
                         <!-- Temporary nav item -->
                     </li>
                 </ul>
@@ -54,58 +70,117 @@ if (empty($_SESSION['email'])){
         </div>
     </nav>
     <main class="container">
-        <div class="container-fluid mb-5">
-            <!-- iwan nyo to para sa carousel -->
+        <div class="container-fluid mt-3 mb-4">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="../ecommerce/src/img/banner1.PNG" class="d-flex w-100" alt="banner1">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../ecommerce/src/img/banner2.PNG" class="d-flex w-100" alt="banner2">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../ecommerce/src/img/banner3.PNG" class="d-flex w-100" alt="banner3">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
         <div class="card-wrapper d-flex flex-column align-items-center" style="height: 65vh;">
             <span class="mb-4 d-flex align-items-center justify-content-start w-75">
-                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="white" class="bi bi-search" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="white" class="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg>
                 <input type="text" name="" placeholder="Search...">
             </span>
-            <div class="card mt-2 p-5 d-flex align-items-center" style="border:none; border-radius: 0; height: 80%;">
+            <div class="card product-wrapper mt-2 p-lg-5 p-xxl-5 p-sm-0 p-md-0 d-flex align-items-center w-75" style="border:none; border-radius: 0; height: auto;">
                 <div class="row g-5">
-                    <div class="col-lg-4">
-                        <a href="melt_tee.php">
+                    <div class="col-lg-4 col-sm-12 text-center">
+                        <a href="melt_tee.php" class="text-dark" style="text-decoration: none;">
                             <?php
                             $select_melt_tee = "SELECT * FROM products WHERE product_id = '84562983'";
                             $query_melt_tee = mysqli_query($conn, $select_melt_tee);
                             $rows = mysqli_fetch_array($query_melt_tee);
                             ?>
-                        <a href="melt_tee.php?pid=<?php echo $rows['product_id'];?>">
                         <img src="src/img/<?php echo $rows['image'];?>" height="250px" width="200px" alt="">
                         <span class="text-center">
-                            <small><h6 class="p-0 m-0"><?php echo $rows['name'];?></h6></small>
-                            <small><h6 class="p-0 m-0">₱ <?php echo number_format($rows['price'],2);?></h6></small>
+                            <small><h6 class="mt-2 mb-0"><?php echo $rows['name'];?></h6></small>
+                            <small><h6 class="p-0">₱ <?php echo number_format($rows['price'],2);?></h6></small>
                         </span>
                         </a>
                     </div>
-                    <div class="col-lg-4">
-                        <a href="take_risk_dye.php">
+                    <div class="col-lg-4 col-sm-12 text-center">
+                        <a href="take_risk_dye.php" class="text-dark" style="text-decoration: none;">
                         <?php
                             $select_takerisk = "SELECT * FROM products WHERE product_id = '82758426'";
                             $query_takerisk = mysqli_query($conn, $select_takerisk);
                             $rows = mysqli_fetch_array($query_takerisk);
                             ?>
-                        <a href="take_risk_dye.php">
                         <img src="src/img/<?php echo $rows['image'];?>" height="250px" width="200px" alt="">
                         <span class="text-center">
-                            <small><h6 class="p-0 m-0"><?php echo $rows['name'];?></h6></small>
+                            <small><h6 class="p-0 mt-2 mb-0"><?php echo $rows['name'];?></h6></small>
                             <small><h6 class="p-0 m-0">₱ <?php echo number_format($rows['price'],2);?></h6></small>
                         </span>
                         </a>
                     </div>
-                    <div class="col-lg-4">
-                        <a href="kedrick_bootleg.php">
+                    <div class="col-lg-4 text-center">
+                        <a href="kedrick_bootleg.php" class="text-dark" style="text-decoration: none;">
                         <?php
                             $select_kendrick = "SELECT * FROM products WHERE product_id = '38175967'";
                             $query_kendrick = mysqli_query($conn, $select_kendrick);
                             $rows = mysqli_fetch_array($query_kendrick);
                             ?>
-                        <a href="kedrick_bootleg.php">
                         <img src="src/img/<?php echo $rows['image'];?>" height="250px" width="200px" alt="">
                         <span class="text-center">
-                            <small><h6 class="p-0 m-0"><?php echo $rows['name'];?></h6></small>
+                            <small><h6 class="p-0 mt-2 mb-0"><?php echo $rows['name'];?></h6></small>
+                            <small><h6 class="p-0 m-0">₱ <?php echo number_format($rows['price'],2);?></h6></small>
+                        </span>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 text-center">
+                        <a href="WICO_hoodie.php" class="text-dark" style="text-decoration: none;">
+                        <?php
+                            $select_WICO = "SELECT * FROM products WHERE product_id = '83721543'";
+                            $query_WICO = mysqli_query($conn, $select_WICO);
+                            $rows = mysqli_fetch_array($query_WICO);
+                            ?>
+                        <img src="src/img/<?php echo $rows['image'];?>" height="250px" width="200px" alt="">
+                        <span class="text-center">
+                            <small><h6 class="p-0 mt-2 mb-0"><?php echo $rows['name'];?></h6></small>
+                            <small><h6 class="p-0 m-0">₱ <?php echo number_format($rows['price'],2);?></h6></small>
+                        </span>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 text-center">
+                        <a href="Frank_Ocean.php" class="text-dark" style="text-decoration: none;">
+                        <?php
+                            $select_frank = "SELECT * FROM products WHERE product_id = '67251438'";
+                            $query_frank = mysqli_query($conn, $select_frank);
+                            $rows = mysqli_fetch_array($query_frank);
+                            ?>
+                        <img src="src/img/<?php echo $rows['image'];?>" height="250px" width="200px" alt="">
+                        <span class="text-center">
+                            <small><h6 class="p-0 mt-2 mb-0"><?php echo $rows['name'];?></h6></small>
+                            <small><h6 class="p-0 m-0">₱ <?php echo number_format($rows['price'],2);?></h6></small>
+                        </span>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 text-center">
+                        <a href="Miata.php" class="text-dark" style="text-decoration: none;">
+                        <?php
+                            $select_miata = "SELECT * FROM products WHERE product_id = '62514837'";
+                            $query_miata = mysqli_query($conn, $select_miata);
+                            $rows = mysqli_fetch_array($query_miata);
+                            ?>
+                        <img src="src/img/<?php echo $rows['image'];?>" height="250px" width="200px" alt="">
+                        <span class="text-center">
+                            <small><h6 class="p-0 mt-2 mb-0"><?php echo $rows['name'];?></h6></small>
                             <small><h6 class="p-0 m-0">₱ <?php echo number_format($rows['price'],2);?></h6></small>
                         </span>
                         </a>
@@ -117,6 +192,7 @@ if (empty($_SESSION['email'])){
     <!-- <h1>Home</h1>
     welcome user
     <a href="logout.php">logout</a> -->
-
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
--- melt_tee.php
+++ melt_tee.php
@@ -5,14 +5,6 @@ if (empty($_SESSION['email'])){
     header("location:login.php");
     exit();
 }
-if($_GET['pid']){
-    $product_id = $_GET['pid'];
-}
-    $email = $_SESSION['email'];
-    $select_userid = "SELECT * FROM `user` WHERE email = '$email'";
-    $query_userid = mysqli_query($conn, $select_userid);
-    $rows = mysqli_fetch_array($query_userid);
-    $user_id = $rows['user_id'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,96 +13,120 @@ if($_GET['pid']){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bs-5/bootstrap/dist/css/bootstrap.css">
-    <title>Coozy Apparel.</title>
+    <title>Coozy Apparel</title>
 </head>
 <body class="bg-maroon">
     <nav class="navbar navbar-expand-lg navbar-dark bg-black">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="../ecommerce/src/img/logo.png" width="160" alt="" sizes="" srcset="">
+                <img src="../ecommerce/src/img/logo.png" width="140" alt="" sizes="" srcset="">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto" style="font-size: 20px;">
+                <ul class="navbar-nav ms-auto" style="font-size: 19px;">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Shop</a>
+                        <a class="nav-link" href="home.php">SHOP</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Cart</a>
+                        <a class="nav-link" href="#">CART</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
+                            PRODUCTS
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">New Arrival</a>
+                            <a class="dropdown-item" href="#">Sale</a>
+                            <a class="dropdown-item" href="#">Hot</a>
+                            <a class="dropdown-item" href="#">T-Shirts</a>
+                            <a class="dropdown-item" href="#">Bags</a>
+                        </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Products</a>
+                        <a class="nav-link" href="#">ORDER STATUS</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Order Status</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="logout.php">Log Out</a>
+                    <li class="nav-item dropdown align-items-center d-flex">
+                        <a class="nav-link py-0 dropdown-toggle" href="#" id="navbarDropdown" data-bs-toggle="dropdown"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                        </svg></a>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="settings.html">Settings</a>
+                            <a class="dropdown-item" href="logout.php">Logout</a>
+                        </div>
                         <!-- Temporary nav item -->
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
-    <main class="container">
-    <div class="card mt-5 p-5 d-flex align-items-left" style="border:none; border-radius: 0; height: 80%;">
-                <div class="row g-5">
-                    <div class="col-lg-4">
-                        <form action="" method="POST">
-                        <?php
-                            $select_melt_tee = "SELECT * FROM products WHERE product_id = '84562983'";
-                            $query_melt_tee = mysqli_query($conn, $select_melt_tee);
-                            $rows = mysqli_fetch_array($query_melt_tee);
-                            ?>
-                        <img src="src/img/<?php echo $rows['image'];?>" height="250px" width="200px" alt="">
-                        <img src="src/img/melttee_chart.png" height="250px" width="450px" alt="">
-                        <span class="text-left">
-                            <small><h6 class="p-0 m-2"><?php echo $rows['name'];?></h6></small>
-                            <small><h6 class="p-0 m-2">₱ <?php echo number_format($rows['price'],2);?></h6></small>
-                                <select name="size" id="">
-                                    <option value="">SIZES</option>
-                                    <option value="Medium">Medium</option>
-                                    <option value="Large">Large</option>
-                                    <option value="X-Large">X-Large</option>
-                                    <option value="XX-Large">XX-Large</option>
-                                </select>
-                                <input type="number" name="quantity" min="0" max="50" placeholder="QTY">
-                                <button type="submit" name="submit">ADD TO CART</button>
-                        </span>
-                        </form>
+    <main class="container d-flex justify-content-center mt-5">
+        <img src="../ecommerce/src/img/Melt Tee.PNG" class="m-4" height="400px" width="290px" alt="">
+            <div class="container-fluid row" style="width: 30%;">
+                <!-- card start -->
+                <div class="card product-wrapper mt-4 px-3 py-4 col-12 mb-0 pb-0 border border-dark" style="border:none; border-radius: 0; height: 28vh;">
+                    <div class="row d-flex">
+                        <div class="col-lg-12 col-sm-12 text-left">
+                            <div class="row">
+                                <a href="melt_tee.php" class="text-dark" style="text-decoration: none;">
+                                    <?php
+                                    $select_melt_tee = "SELECT * FROM products WHERE product_id = '84562983'";
+                                    $query_melt_tee = mysqli_query($conn, $select_melt_tee);
+                                    $rows = mysqli_fetch_array($query_melt_tee);
+                                    ?>
+                                    <span class="text-center">
+                                        <small><h5 class="p-0 mt-0"><?php echo $rows['name'];?> ₱ <?php echo number_format($rows['price'],2);?></h5></small>
+                                    </span>    
+                                </a>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-6">
+                                    <select class="form-select border border-dark" name="" id="">
+                                        <option value="">Sizes</option>
+                                        <option value="">Medium</option>
+                                        <option value="">Large</option>
+                                        <option value="">XL</option>
+                                        <option value="">2XL</option>
+                                    </select>
+                                </div>
+                                <div class="col-6">
+                                    <select class="form-select border border-dark" name="" id="">
+                                        <option value="">Quantity</option>
+                                        <option value="">1</option>
+                                        <option value="">2</option>
+                                        <option value="">3</option>
+                                        <option value="">4</option>
+                                        <option value="">5</option>
+                                        <option value="">6</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="btn">
+                                    <button type="button" class="btn btn-dark text-white">CANCEL</button>
+                                    <button type="button" class="btn btn-warning text-black">ADD TO CART</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    </div>
+                </div>
+                <!-- card end -->
+                <div class="col-12 text-white border border-dark">
+                    <p>Cop this first easeful design Ice Melt Tee cooz! 
+                        Keep it Cool and Cozy!
+                        and also follow us on
+                        FB: CoozyAppparelCo
+                        IG: CoozyApparelCo</p>
+                </div>
+            </div>
+        </div>
     </main>
     <!-- <h1>Home</h1>
     welcome user
     <a href="logout.php">logout</a> -->
-
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
-<?php
-    if(isset($_POST['submit'])){
-        $size = $_POST['size'];
-        $quantity = $_POST['quantity'];
-        
-        if($size == ""){
-            echo '<script>alert("Please pick a product size")</script>';
-            exit();
-        }
-        if(empty($quantity)){
-            echo '<script>alert("Please input quantity")</script>';
-            exit();
-        }
-
-        $add_to_cart = "INSERT INTO `user_orders`(`user_id`, `product_id`, `quantity`, `size`, `price`, `status`) 
-        VALUES ('$user_id','$product_id','$quantity','$size','450.00','Pending Cart')";
-        $query_add_to_cart = mysqli_query($conn, $add_to_cart);
-        if($query_add_to_cart){
-            echo '<script>alert("Product added to cart")</script>';
-            exit();
-        }else{
-            echo '<script>alert("Something went wrong")</script>';
-        }
-    }
-?>
