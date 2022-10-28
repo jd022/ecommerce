2aec24c03ff40fc81a7654f1cba8ea56055a1df0
Jade Mark <88232304+jd022@users.noreply.github.com>
2022-10-29 02:46:14 +0800

    update 10/29/22

---
+++ admin/orders.php
@@ -0,0 +1,206 @@
+<?php
+include ("../connection.php");
+session_start();
+if (empty($_SESSION['email'])){
+    header("location:login.php");
+    exit();
+}else{
+    $email = $_SESSION['email'];
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bs-5/bootstrap/dist/css/bootstrap.css">
+    <title>Coozy Apparel</title>
+</head> 
+<style>
+    @media screen and (width: 992) {
+        .card-wrapper{
+            width: 100%;
+        }
+    }
+    .row-divider{
+        border: 1px solid black;
+        background: black;
+        height: 100%;
+    }
+</style>
+<body class="bg-maroon">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-black">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="../src/img/logo.png" width="150" alt="" sizes="" srcset="">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto" style="font-size: 20px;">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">DASHBOARD</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">SALES</a>
+                    </li>
+                    <li class="nav-item dropdown align-items-center d-flex">
+                        <a class="nav-link py-0 dropdown-toggle" href="#" id="navbarDropdown" data-bs-toggle="dropdown"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                        </svg></a>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="../logout.php">Logout</a>
+                        </div>
+                        <!-- Temporary nav item -->
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main class="container">
+        <div class="card-wrapper d-flex flex-column align-items-center" style="height: 65vh;">
+            <div class="card product-wrapper mt-5 p-lg-5 p-xxl-5 p-sm-0 p-md-0 w-75" style="text-align:justify; border:none; border-radius: 0; height: auto;">
+                <h2>CUSTOMER ORDERS</h2>
+                <span><a href="orders.php?p">PENDING</a>
+                <a href="orders.php?td">TO DELIVER</a>
+                <a href="orders.php?h">HISTORY</a></span>
+                <?php
+                if(isset($_GET['h'])){
+                ?>
+                <?php
+                $deliver_order = "SELECT * FROM user_orders 
+                WHERE status = 'Delivered' ORDER BY date_time_created DESC";
+                $query_deliver_order = mysqli_query($conn, $deliver_order);
+                if($row_count = mysqli_num_rows($query_deliver_order) > 0){
+                ?>
+                <table style="text-align: center;">
+					<thead>
+					<tr>
+					<th>NO</th>
+                    <th>ORDER ID</th>
+                    <th>DATE OF ORDER</th>
+                    <th>Operation</th>
+					</tr>
+					</thead>
+				
+					<?php 
+							while($rows = mysqli_fetch_array($query_deliver_order)){
+					?>
+					<tbody>
+					<tr>
+                    <td><?php echo $row_count;?></td>
+					<td><?php echo $rows['order_id'];?></td>
+					<td><?php echo date("F d, Y h:i:s A", strtotime($rows['date_time_created']));?></td>
+                    <td><a class="confirm-buton" href="#">ACCEPT</a>
+                        <a href="">REJECT</a>
+                    </td>
+					</tr>
+					</tbody>
+					<?php
+					}
+					?>
+					</table>
+                <?php
+                }else{
+                    echo '<h1>No Order</h1>';
+                }
+                ?>
+                <?php
+                }else if(isset($_GET['td'])){
+                ?>
+                <?php
+                $deliver_order = "SELECT * FROM user_orders 
+                WHERE status = 'To Deliver' ORDER BY date_time_created DESC";
+                $query_deliver_order = mysqli_query($conn, $deliver_order);
+                if($row_count = mysqli_num_rows($query_deliver_order) > 0){
+                ?>
+                <table style="text-align: center;">
+					<thead>
+					<tr>
+					<th>NO</th>
+                    <th>ORDER ID</th>
+                    <th>DATE OF ORDER</th>
+                    <th>Operation</th>
+					</tr>
+					</thead>
+				
+					<?php 
+							while($rows = mysqli_fetch_array($query_deliver_order)){
+					?>
+					<tbody>
+					<tr>
+                    <td><?php echo $row_count;?></td>
+					<td><?php echo $rows['order_id'];?></td>
+					<td><?php echo date("F d, Y h:i:s A", strtotime($rows['date_time_created']));?></td>
+                    <td><a class="confirm-buton" href="#">ACCEPT</a>
+                        <a href="">REJECT</a>
+                    </td>
+					</tr>
+					</tbody>
+					<?php
+					}
+					?>
+					</table>
+                <?php
+                }else{
+                    echo '<h1>No Order</h1>';
+                }
+                ?>
+                <?php
+                }else{
+                ?>
+                <?php
+                $pending_order = "SELECT * FROM user_orders 
+                
+                WHERE status = 'Pending'";
+                $query_pending_order = mysqli_query($conn, $pending_order);
+                if($row_count = mysqli_num_rows($query_pending_order) > 0){
+                ?>
+                <table style="text-align: center;">
+					<thead>
+					<tr>
+					<th>NO</th>
+                    <th>ORDER ID</th>
+                    <th>DATE OF ORDER</th>
+                    <th>Operation</th>
+					</tr>
+					</thead>
+				
+					<?php 
+							while($rows = mysqli_fetch_array($query_pending_order)){
+					?>
+					<tbody>
+					<tr>
+                    <td><?php echo $row_count;?></td>
+					<td><?php echo $rows['order_id'];?></td>
+					<td><?php echo date("F d, Y h:i:s A", strtotime($rows['date_time_created']));?></td>
+                    <td><a class="confirm-buton" href="#">ACCEPT</a>
+                        <a href="">REJECT</a>
+                    </td>
+					</tr>
+					</tbody>
+					<?php
+					}
+					?>
+					</table>
+                    <?php
+                    }else{
+                        echo '<h1>No Order</h1>';
+                    }
+                    ?>
+                <?php
+                }
+                ?>
+            </div>
+        </div>
+    </main>
+    <!-- <h1>Home</h1>
+    welcome user
+    <a href="logout.php">logout</a> -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
--- cart.php
+++ cart.php
@@ -22,15 +22,17 @@ if (empty($_SESSION['email'])){
 </head>
 <body class="bg-maroon">
     <?php include 'includes/nav.php';?>
+    <form action="" method="POST">
     <main class="container px">
     <div class="card mt-5 mx-lg-5 p-5 d-flex align-items-center" style="border:none; border-radius: 0; height: 80%;">
             <table class="table">
                 <tbody>
                 <?php
                 $status = 'Cart';
-                $sql = "SELECT user_orders.user_id, user_orders.order_id, user_orders.product_id, user_orders.quantity, user_orders.size,
+                $sql = "SELECT products.price as p_price, user_orders.user_id, user_orders.order_id, user_orders.product_id, user_orders.quantity, user_orders.size,
                 user_orders.status, user_orders.price, products.product_id, products.image, products.name
-                FROM `user_orders` LEFT JOIN products on products.product_id = user_orders.product_id
+                FROM `user_orders` 
+                LEFT JOIN products on products.product_id = user_orders.product_id
                 WHERE user_id = '$user_id' AND status = '$status'";
                 $result = mysqli_query($conn, $sql) or die (mysqli_error($con));
                 if(mysqli_num_rows($result) > 0){
@@ -44,18 +46,18 @@ if (empty($_SESSION['email'])){
                         </td>
                         <td>
                             <span>
-                                <p class="h2 p-0 m-0"><?= $rows['name'];?></p>
+                                <p class="h2 p-0 m-0"><?php echo $rows['name'];?></p>
                                 <p class="h5 p-0 m-0"><?php echo $rows['size'];?></p>
                                 <span class="d-flex">
-                                    <p>₱</p>
-                                    <p id="price"><?php echo $rows['price'];?></p>
+                                    <p>₱ </p>
+                                    <p id="price"><?php echo $rows['p_price'];?></p>
                                 </span>
                             </span>
                         </td>
                         <td>
                             <span>
                                 <p class="m-0 text-center">Quantity</p>
-                                <input type="number" class="form-control" id="qty" min="0" max="99" value="<?php echo $rows['quantity'];?>">
+                                <input type="number" name="quantity" class="form-control" id="qty" min="0" max="99" value="<?php echo $rows['quantity'];?>">
 
                             </span>
                         </td>
@@ -70,12 +72,13 @@ if (empty($_SESSION['email'])){
                     <div class="container bg-light p-5">
                         <span class="h5">Mode of Payment: Cash on Delivery</span>
                         <span class="d-flex h5">
-                            <p>Total: ₱</p>
+                            <p>Total: ₱&nbsp;</p>
                             <p class="total"></p>
                         </span>
                         <span>
-                            <a class="btn btn-primary" href="cart.php?c=<?php echo $user_id;?>">CHECK OUT</a>
+                            <button type="submit" name="submit" class="btn btn-primary">CHECK OUT</button>
                         </span>
+                        </form>
                     </div>
                     <?php
                 }else{
@@ -106,8 +109,9 @@ function upAmount(){
     $('tbody #cart').each(function(){
         var qty = $(this).find('#qty').val();
         var price = $(this).find('#price').text();
-        var amount = (qty*price)
+        var amount = (qty*price);
         sum+=amount;
+        
     })
     $('.total').text(sum);
 }
@@ -115,14 +119,19 @@ function upAmount(){
 <?php include 'includes/footer.php';?>
 
 <?php
-    if(isset($_GET['c'])){
+    if(isset($_POST['submit'])){
+        $quantity = $_POST['quantity'];
         $date = date('ym');
         $rand = rand('0000', '9999');
         $otp = "".$date."".$rand."";
 
-        $check_order_id = "SELECT * FROM user_orders WHERE id ORDER BY order_id DESC";
+        $check_order_id = "SELECT *, products.price as p_price
+        FROM user_orders
+        LEFT JOIN products on products.product_id = user_orders.product_id
+        WHERE user_orders.id ORDER BY user_orders.order_id DESC";
         $query_order_id = mysqli_query($conn, $check_order_id);
         $row = mysqli_fetch_array($query_order_id);
+        $p_price = $row['p_price'];
         $last_id = $row['order_id'];
         if(empty($last_id)){
             $order_item = "00". $date . $rand;
@@ -132,10 +141,12 @@ function upAmount(){
         }
 
         date_default_timezone_set('Asia/Manila');
-		$date_time_updated = date("Y-m-d H:i:s");
-		
-		$update_user_order = "UPDATE `user_orders` SET `order_id` = '$order_item', `status` = 'Pending'
-        WHERE user_id = '$user_id' and `status` = 'Cart'";
+		$date_time_created = date("Y-m-d H:i:s");
+		$total_price = $quantity * $p_price;
+
+		$update_user_order = "UPDATE `user_orders` SET `order_id` = '$order_item', `status` = 'Pending', 
+        `quantity` = '$quantity', `price` = '$total_price', `date_time_created` = '$date_time_created'
+        WHERE user_id = '$user_id' AND `status` = 'Cart'";
         $query_update_user_order = mysqli_query($conn, $update_user_order);
         if($query_update_user_order == true){
             echo "<script>alert('Order submitted, check your order status to check your orders.');
