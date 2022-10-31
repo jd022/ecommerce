486ad831fc05d4c0e71af3022eed9be8a136c6c3
kirstenmra <92195526+kirstenmra@users.noreply.github.com>
2022-11-01 01:48:35 +0800

    update index.php
    limit sa password

--- index.php
+++ index.php
@@ -109,14 +109,14 @@ use PHPMailer\PHPMailer\Exception;
                                     <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
                                 </svg>
                                 <input type="text" 
-                                class="py-1 mx-2 w-100" name="email" style="font-weight: 800;" maxlength="50" placeholder="Email">
+                                class="py-1 mx-2 w-100" name="email" style="font-weight: 800;" maxlength="30" placeholder="Email">
                             </span>
                             <span class="col-lg-12 hstack mb-4 px-4">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-shield-lock-fill" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5z"/>
                                 </svg>
                                 <input type="password"
-                                class="py-1 mx-2 w-100" name="password" style="font-weight: 800;" maxlength="50" placeholder="Password">
+                                class="py-1 mx-2 w-100" name="password" style="font-weight: 800;" maxlength="30" placeholder="Password">
                             </span>
                             <span class="col-8 px-3 d-flex flex-column align-items-start">
                                 <small><a href="retrieve.php" class="text-dark text-decoration-none text-start w-100" style="font-weight: 900;">Forgot Password?</a></small>
